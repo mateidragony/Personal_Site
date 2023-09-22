8f5e5d4465c7fcc6ab286e179738760fa3e32fe1
mateidragony <matei.cloteaux@gmail.com>
2023-09-22 17:55:23 -0400

    Add static files

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(express.json({extended: false}));
 app.use('/', require("./routes/index"));
 app.use('/api/url', require("./routes/url"));
 
+app.use(express.static("assets"));
+
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
