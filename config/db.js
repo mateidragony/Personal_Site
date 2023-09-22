f17481b0adbc5ae778d610bd0ae5ef5a1ebdc0a0
mateidragony <matei.cloteaux@gmail.com>
2023-09-22 17:05:56 -0400

    Back to this :(

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const config = require("/models/default.json");
+const config = require("./default.json");
 const db = config["mongoURI"];
 
 const connectDB = async () => {
