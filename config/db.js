10cc49b1ca44a5fa6e4f5dd7a68c8ecce5ea5bf7
mateidragony <matei.cloteaux@gmail.com>
2023-09-22 17:15:11 -0400

    Burhaps

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const config = require("./default.json");
-const db = config["mongoURI"];
+const db = process.env.MONGO_URI;
 
 const connectDB = async () => {
     try {
--- routes/url.js
+++ routes/url.js
@@ -11,7 +11,7 @@ const Url = require("../models/Url");
 // @desc    Create short URL
 router.post('/shorten', async (req, res) => {
     const {longUrl} = req.body;
-    const baseUrl = config.get("baseUrl");
+    const baseUrl = window.location.href;
 
     if(!validUrl.isUri(baseUrl)){
         return res.status(401).json('Invalid base url');
